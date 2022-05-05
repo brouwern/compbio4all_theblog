687c01a108a27be094aa6ff5acb8741d57a15fe5
Nathan Brouwer <brouwern@gmail.com>
2022-05-05 13:44:59 -0400

    bongers

---
+++ _notes.R
@@ -0,0 +1,20 @@
+
+# Notes
+
+
+# render
+rmarkdown::render_site(encoding = 'UTF-8')
+
+
+# https://www.linkedin.com/in/nathan-brouwer-phd-97100980/
+# https://www.biology.pitt.edu/
+
+# new post
+distill::create_post(
+  title = "Scatterplot case study: Ecological succession in Mexico", # mandatory
+  author = "Nathan L. Brouwer",
+  slug = "auto", # generates a website slug (URL)
+  date_prefix = TRUE, # adds date for sorting
+  draft = FALSE, 
+  edit = interactive()
+)
