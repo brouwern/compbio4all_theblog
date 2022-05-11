752cbcd36f60e6fbd39488fbae4e43b1e41aa06e
Nathan Brouwer <brouwern@gmail.com>
2022-05-11 00:45:18 -0400

    test webex

--- _notes.R
+++ _notes.R
@@ -13,13 +13,29 @@ rmarkdown::render_site(encoding = 'UTF-8')
 
 # new post
 distill::create_post(
-  title = "Blood feud: ggplot versus base R for beginers", # mandatory
+  title = "webexercises test", # mandatory
   author = "Nathan L. Brouwer",
   slug = "auto", # generates a website slug (URL)
   date_prefix = TRUE, # adds date for sorting
   draft = FALSE, 
   edit = interactive()
 )
+
+# ---
+#   title: "Blood feud: ggplot versus base R for beginers"
+# description: |
+#   ggplot2 is powerful, but is it appropriate for complete beginners?
+#   author: Nathan L. Brouwer
+# date: 2022-05-05
+# output:
+#   distill::distill_article:
+#   self_contained: false
+# draft: false
+# categories:
+#   - ggplot2 versus R
+# - base R
+# - time series
+# ---
 
 
 # https://rstudio.github.io/distill/blog_workflow.html
