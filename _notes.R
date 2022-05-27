149e0565d6afff9d92c6b8e38b77b47cc156b0c2
Nathan Brouwer <brouwern@gmail.com>
2022-05-27 12:39:08 -0400

    update

--- _notes.R
+++ _notes.R
@@ -1,62 +1,30 @@
 
 # Notes
 
-# https://themockup.blog/posts/2020-08-01-building-a-blog-with-distill/
+# 
 
 
 # render
-rmarkdown::render_site(encoding = 'UTF-8')
+
 
 
 # https://www.linkedin.com/in/nathan-brouwer-phd-97100980/
 # https://www.biology.pitt.edu/
 
 # new post
-distill::create_post(
-  title = "Latex equations for biologists from A to Z", # mandatory
-  author = "Nathan L. Brouwer",
-  slug = "auto", # generates a website slug (URL)
-  date_prefix = TRUE, # adds date for sorting
-  draft = FALSE, 
-  edit = interactive()
-)
+
+
+
+# Stuff for each post's YAML
+
+  
+
+
 
 # 
-# ---
-#   title: "Welcome to Computational Biology for All!"
-# description: |
-#   Welcome to my new, Computational Biology for All!
-#   author:
-#   - name: Nathan L. Brouwer
-# url: https://www.linkedin.com/in/nathan-brouwer-phd-97100980/
-#   affiliation: University of Pittsburgh
-# affiliation_url: https://www.biology.pitt.edu/
-#   date: 05-05-2022
-# output:
-#   distill::distill_article:
-#   self_contained: false
-# ---
-  
-  
-# ---
-#   title: "Blood feud: ggplot versus base R for beginers"
-# description: |
-#   ggplot2 is powerful, but is it appropriate for complete beginners?
-#   author: Nathan L. Brouwer
-# date: 2022-05-05
-# output:
-#   distill::distill_article:
-#   self_contained: false
-# draft: false
-# categories:
-#   - ggplot2 versus R
-#   - base R
-#   - time series
-# ---
 
 
-# https://rstudio.github.io/distill/blog_workflow.html
+```{r, echo = F}
 
+```
 
-# shortening fASTA seq
-substring()
